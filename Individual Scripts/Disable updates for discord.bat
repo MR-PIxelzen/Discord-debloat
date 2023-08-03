5d93264eb15b53735130ea2dc3fee7ce6c87854c
PiXZeN <120831803+MR-PIxelzen@users.noreply.github.com>
2023-08-03 23:36:18 +0530

    Add files via upload

---
+++ Individual Scripts/Disable updates for discord.bat
@@ -0,0 +1,49 @@
+@echo off
+@echo disable updates for discord
+@echo off
+DEL "%userprofile%\appdata\Local\discord\Update.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9019\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9018\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9017\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9016\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9015\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9014\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9013\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9012\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9010\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9008\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9007\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9006\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9005\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9004\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9003\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9002\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9001\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-0.0.309\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-0.0.308\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-0.0.307\SquirrelSetup.log" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-0.0.306\SquirrelSetup.log" /F /Q
+REM DELETES Squirrel.exe
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9019\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9018\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9017\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9016\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9015\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9014\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9013\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9012\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9010\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9008\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9007\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9006\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9005\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9004\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9003\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9002\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-1.0.9001\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-0.0.309\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-0.0.308\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-0.0.307\Squirrel.exe" /F /Q
+DEL "%userprofile%\appdata\Local\discord\app-0.0.306\Squirrel.exe" /F /Q
+
+pause
